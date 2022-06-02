19e8c7afc2913ae78f7eb2303beb18b0afbd57d9
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-06-02 09:13:53 -0400

    Variables, Settings and Setup (1)

--- Sketch.java
+++ Sketch.java
@@ -1,12 +1,23 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-  // Global Variables
-  int Height = 300;
-  int Width = 300;
-  int initScreen;
-  int gameScreen = 0;
-  int gameOverScreen;
+  // Game Settings
+ int x = 250;
+ int y = 350;
+ int speedX = 4;
+ int speedY = -4;
+ int paddleX = 250;
+ int paddleY = 350;
+ int paddle_width_half = 45;
+ int score = 0;
+ int level = 1;
+ int waitTime = 1;
+ int i;
+ int bricks;
+
+
+
+
   
 
 	
@@ -16,38 +27,25 @@ public class Sketch extends PApplet {
   public void settings() {
 	// put your size call here
     size(400, 400);
+    rectMode(CENTER);
+    for (int i = 0; i<20; i++) {
+      bricks{i} = 2;      
   }
+}
+
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(0);
+    background(0, 255, 204);
+  
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    if (gameScreen == 0);
   }
-
-  public void initScreen() {
-  }
-
-  public void gameScreen() {
-  }
-
-  public void gameOverScreen() {
-  }
-
-  public void startGame() {
-    gameScreen=1;
-  }
-
-  public void mousePressed() {
-  }
-}  
-
-  
+}
