a71bcaab281e5062b2fe04a07ddda03d361a4ed9
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-06-02 20:25:57 -0400

    Void Draw Code (1)

--- Sketch.java
+++ Sketch.java
@@ -1,51 +1,52 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-  // Game Settings
+  // Game Variables
+
+ float[]brickX = new float[20];
  int x = 250;
  int y = 350;
  int speedX = 4;
  int speedY = -4;
  int paddleX = 250;
  int paddleY = 350;
- int paddle_width_half = 45;
  int score = 0;
  int level = 1;
  int waitTime = 1;
  int i;
- int bricks;
+ int x_Temp;
+ int y_Temp;
+ int blocks_Gone;
+ 
 
-
-
-
-  
-
-	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
     rectMode(CENTER);
-    for (int i = 0; i<20; i++) {
-      bricks{i} = 2;      
-  }
-}
-
+    }
+  
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(0, 255, 204);
-  
+    background(0, 0, 0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    x = x + speedX;
+    y = y + speedY;
+
+    if (x>width || x<0)  
+    speedX = -speedX;
+
+  if (y<0)
+    speedY = -speedY;
   }
-}
