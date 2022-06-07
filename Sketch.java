9ece35436980b27fde6755c846ff0ff8161fd1dd
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-06-07 16:08:03 -0400

    Different Screens

--- Sketch.java
+++ Sketch.java
@@ -4,8 +4,8 @@ public class Sketch extends PApplet {
   // Global Variables
 
   // Ball Variables
-  int ballX = 400;
-  int ballY = 300;
+  float ballX = 400;
+  float ballY = 300;
   float ballSpeed = 2;
   float ballDirectionX = -2;
   float ballDirectionY = -2;
@@ -22,20 +22,26 @@ public class Sketch extends PApplet {
   int playerHeight = 25;  
 
   // Speed of Player
-  int player1Speed = 4;
+  int player1Speed = 6;
 
   // Scoreboard
   int score;
+  int player1Score;
 
   // Keyboard Variables
   int keyPressed;
+
+  // 
+
+  // In Game Variables
+  int stage = 0;
 
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(800, 450);
+    size(800, 500);
     }
   
 
@@ -48,6 +54,7 @@ public class Sketch extends PApplet {
     background(255, 59, 20);
     rectMode(CENTER);
     imageMode(CENTER);
+    textAlign(CENTER);
     //close setup
   }
 
@@ -57,52 +64,22 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 
-  // Soccer Field
-  background(52, 235, 94);
-  noFill();
-  stroke(255);
-  strokeWeight(4);
-  rect(400, 250, 800, 500);
-	line(400, 0, 400, 500); 
-
-  // Soccer Ball
-  noStroke();
-  fill(255);
-  rect(ballX, ballY, 25, 25);
-
-  // Player
-  noStroke();
-  fill(255, 45, 8);
-  rect(player1X, player1Y, playerWidth, playerHeight);
-
-  // Net
-  fill(0);
-
-}
-
-
-  public void keyPressed() {
-    if (key == 'w') {
-     player1Y = player1Y -player1Speed;
-     	}//close w pressed
-       
-    if (key == 'a') {
-      player1Y = player1Y + player1Speed;
-      }//close a pressed
-
-    if (key == 's') {
-      player1X = player1X - player1Speed;
-      }//close s pressed
+    if(stage == 0){
+      splash();
+    }//close 0
     
-    if (key == 'd') {
-      player1X = player1X + player1Speed;
-      }//close d pressed
+    if(stage == 1){ //easy
+      game(); //run game
+    }//close 1
+    
+    
+    if(stage == 2){
+      player1Wins();
+    }//close = 2
+    
+  }//close draw
+
   
-
-    
-
-
-}
 }
 
                                                                                                   
