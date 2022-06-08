f610e0330d77a3132a5a8359355dc1ad0266e1cc
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-06-08 15:39:24 -0400

    Soccer Field Code

--- Sketch.java
+++ Sketch.java
@@ -6,27 +6,27 @@ public class Sketch extends PApplet {
   // Ball Variables
   float ballX = 400;
   float ballY = 300;
-  float ballSpeed = 2;
-  float ballDirectionX = -2;
-  float ballDirectionY = -2;
+  float ballSpeed = 1;
+  float ballDirectionX = -1;
+  float ballDirectionY = -1;
 
 
   // Player Variables
-  int player1X = 25;
-  int player1Y = 250;
-  int player2X = 780;
-  int player2Y = 245;
+  int p1X = 25;
+  int p1Y = 250;
+  int p2X = 780;
+  int p2Y = 245;
 
   // Location of Player
-  int playerWidth = 30;
-  int playerHeight = 25;  
+  int pWidth = 30;
+  int pHeight = 25;  
 
   // Speed of Player
-  int player1Speed = 6;
+  int pSpeed = 6;
 
   // Scoreboard
-  int score;
-  int player1Score;
+  int pScore;
+  int p1Score;
 
   // Wins
   int player1Wins;
@@ -36,8 +36,10 @@ public class Sketch extends PApplet {
   // Keyboard Variables
   int keyPressed;
 
-  // In Game Variables
+  //Other Variables
   int stage = 0;
+
+
 
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -58,63 +60,30 @@ public class Sketch extends PApplet {
     rectMode(CENTER);
     imageMode(CENTER);
     textAlign(CENTER);
-    //close setup
-  }
+}
+
 
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-
-    if(stage == 0){
-      splash();
-    }//close 0
-    
-    if(stage == 1){ //easy
-      game(); //run game
-    }//close 1
-    
-    
-    if(stage == 2){
-      player1Wins();
-    }//close = 2
-    
-  }//close draw
-
-
-  private void player1Wins() {
-  }
-
-
-  private void game() {
-  }
-
-
-  private void splash() {
-   //Main Screen
-	background(235, 230, 247);
-	noStroke();
 	
-	//Name of The Game
-	fill(255,0,0);
-	textSize(60); 
-	text("CHAMPIONS LEAGUE SOLO", 400, 150);
-	
-	//Creator
-	fill(0);
-	textSize(30); 
-	text("PRESENTED BY SOHAIL", 400, 200);
-	
-	//Begin to Play
-	fill(0);
-	textSize(25); 
-	text("CLICK SPACE TO BEGIN GAME", 400, 400);
+	//Soccer Field
+	background(75, 255, 10); 
+	noFill();
+	stroke(255);
+	strokeWeight(2); 
+	rect(400, 250, 800, 500); 
+	line(400, 0, 400, 500); 
 
-}//close splash
-  }
+}
+}
 
-  
+
+
+
+
 
 
                                                                                                   
