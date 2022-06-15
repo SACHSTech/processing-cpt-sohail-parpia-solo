ae8b8cf4fdf62e4b0bd13a350d6b1210a7516ef9
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-06-15 08:44:59 -0400

    Global Variables Code

--- Sketch.java
+++ Sketch.java
@@ -3,25 +3,27 @@ import processing.core.PImage;
 
 public class Sketch extends PApplet {
   // Global Variables
+
+  // Lava
+  int L1X=0;
   
-// Lava
-int L1X=0;
-  
-// Blocks
-int B1=10;
+  // Blocks
+  int B1=10;
 
-// Locations
-int e1=200;
-int e2=200;
+  // Locations
+  int e1=200;
+  int e2=200;
 
-// Keyboard Variables
-boolean keyPressed;
+  // Keyboard Variables
+  boolean keyPressed;
 
-// Screens
-int stage;
+  // Screens
+  int stage;
 
-// Image
-PImage img;
+  // Image
+  PImage img;
+
+
 
 
   
@@ -40,128 +42,14 @@ PImage img;
    */
 
   public void setup() {
-    img = loadImage("Dark City.jpg");
-    img.resize(width, height);
-
-    rectMode(CENTER);
-    textAlign(CENTER);
   }
 
 /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    image(img, 0, 0);
-  
-    if(stage == 0){
-      splash();
-    }
-    
-    if(stage == 1){ 
-      game(); 
-    }
-    }
-  
-
-    private void splash() {
-      
-  //START GAME
-  fill(225, 255, 0);
-  textSize(40); 
-  text("To BEGIN, Press r", 400, 500);
-  textSize(40);
-  text("WELCOME TO LAVA CITY OBSTACLE", 400, 200);
-
-  textAlign(CENTER);
-
-
-}
-
-
-    private void game() {
-      img = loadImage("SUNSET.jpeg");
-  img.resize(width, height);
-
-  //Main Player
-  fill(132, 0, 255);
-  ellipse(e1,e2,30,30);
- {
-  //Falling Lava 
-    fill(255, 0, 0);
-    rect(420,L1X,40,40,40);
-    fill(255, 0, 0);
-    rect(265,L1X,40,40,40);
-    fill(255, 0, 0);
-    rect(580,L1X,40,40,40);
-    L1X = L1X + B1;
-    if(L1X<0||L1X>600)
-   {
-    B1=-B1;
-   }
-   {
-   if(get(e1,e2)==color(114,23,175))
- {
-   e1=100; 
- }
- //Blocks
-   fill(225, 255, 0);
-   rect(320,0,80,430);
-   fill(225, 255, 0);
-   rect(240,600,80,430);
-   fill(225, 255, 0);
-   rect(480,0,80,300);
-   fill(225, 255, 0);
-   rect(400,520,80,430);
-   fill(225, 255, 0);
-   rect(640,0,80,500);
-   fill(225, 255, 0);
-   rect(560,600,80,300);
-   fill(225, 255, 0);
-   rect(720,0,80,80);
- {
- if(get(e1,e2)==color(92,240,228))
- {
-   
-   e1=100; 
- }
- //Finish
- fill(random (0,255), random(0,30), random(0,240));
- rect(720,0,80,80);
- fill(220,250,0);
-
- e1=100; 
- }
- }
- }
-}
-
-  public void keyPressed(){
-{
-  if(keyCode == UP)
-  {e2=e2-5;}
-
-  // To move Down
-   if(keyCode == DOWN)
-  {e2=e2+5;} 
-
-  // To move Right
-  if(keyCode == DOWN)
-  {e1=e1+5;} 
-
-  // To move Left
-  if(keyCode == LEFT)
-  {e1=e1-5;}
-
-  // To begin game
-  if (keyCode == 'r');
-  stage=1;
-
-  
   }
 }
-}
-
-
     
 
 
